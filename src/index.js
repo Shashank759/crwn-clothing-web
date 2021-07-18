50bb89d35aae5f1558dc0424d18ddca974164c57
Shashank Pal <pshashankram@gmail.com>
2021-07-18 19:25:47 +0530

    adding features

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom";
 
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from 'redux-persist/integration/react';
 
 import "./index.css";
 import App from "./App";
@@ -13,13 +14,11 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
